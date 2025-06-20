ab1da5f8f8055dc5b51d0efb2e8cb6d1f3bd853c
Carl Avidano <cavidano@gmail.com>
2025-06-20 14:28:12 -0400

    updating map

---
+++ dist/map-locations.js
@@ -0,0 +1,12 @@
+const correct_map_locations = [
+	{ id: 'namibia', title: 'Namibia', icon: 'officeIcon', city: 'Otjiwarongo, Namibia', popupText: '', latitude: '16.6645', longitude: '-20.4545' },
+	{ id: 'somaliland', title: 'Somaliland', icon: 'officeIcon', city: 'Hargeisa', popupText: '', latitude: '44.065', longitude: '9.56 ' },
+	{ id: 'australia', title: 'Australia', icon: 'affiliateIcon', city: 'Clovelly, New South Wales', popupText: '', latitude: '151.2629', longitude: '-33.9121' },
+	{ id: 'canada', title: 'Canada', icon: 'affiliateIcon', city: 'Carleton Place, Ontario', popupText: '', latitude: '-76.14938', longitude: '45.13341' },
+	{ id: 'france', title: 'France', icon: 'affiliateIcon', city: 'Poissy', popupText: '', latitude: '2.0470', longitude: '48.9296' },
+	{ id: 'italy', title: 'Italy', icon: 'affiliateIcon', city: 'Biella, Italy', popupText: '', latitude: '8.0583', longitude: '45.5628' },
+	{ id: 'netherlands', title: 'Netherlands', icon: 'affiliateIcon', city: 'Tilburg', popupText: '', latitude: '5.091914299999985', longitude: '51.560596' },
+	{ id: 'united-kingdom', title: 'United Kingdom', icon: 'affiliateIcon', city: 'Great Shelford, Cambridge', popupText: '', latitude: '-0.533812', longitude: '51.940125' },
+	{ id: 'united-states', title: 'United States', icon: 'affiliateIcon', city: 'Alexandria, Virginia', popupText: '', latitude: '-77.059325', longitude: '38.818947' },
+	{ id: 'aktionsgemeinschaft-artenschutz-aga-ev', title: 'Aktionsgemeinschaft Artenschutz (AGA) e.V.', icon: 'partnerIcon', city: 'M\u00f6glingen, Germany', popupText: '', latitude: '9.090028', longitude: '48.853635' },
+];
