91c78ff35814ee3c676509c0788a62d01c8eb0f0
Carl Avidano <cavidano@gmail.com>
2025-06-21 14:36:00 -0400

    cleaning up quotes

--- webpack.dev.js
+++ webpack.dev.js
@@ -39,7 +39,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Map}.html`;
+const devPage = `${devPages.Typography}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
